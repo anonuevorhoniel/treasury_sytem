130fde05039df672572e8db3bcbb11469e10153b
anonuevorhoniel <rhon2692@gmail.com>
2026-03-05 14:37:40 +0800

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users = [
-            ['name' => "Jhal", "email" => "jhal@gmail.com", "password" => "jhal123123"],
+            ['name' => "Jhal", "email" => "jhal@gmail.com", "classification" => "Admin", "password" => "jhal123123"],
             ['name' => "Encoder1", "email" => "encoder1@gmail.com", "password" => "encoder1123123"],
             ['name' => "Encoder2", "email" => "encoder2@gmail.com", "password" => "encoder2123123"],
         ];
@@ -24,6 +24,7 @@ class UserSeeder extends Seeder
             User::create([
                 'email' => $user['email'],
                 'name' => $user['name'],
+                'classification' => $user['classification'] ?? "Encoder",
                 'password' => Hash::make($user['password']),
             ]);
         }
